5f79868bcbf7ed77478915cf27866862e650e18a
adriel22 <adriel.arruda@yahoo.com.br>
2019-10-31 14:58:19 -0300

    Add mode to project

---
+++ GitProject/GitProject/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  GitProject
+//
+//  Created by Adriel Freire on 31/10/19.
+//  Copyright © 2019 Adriel Freire. All rights reserved.
+//
+
+import Foundation
+struct Model: Codable {
+    let name: String
+    let age: Int
+}
